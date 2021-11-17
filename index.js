d48c98b8de8b198bee9abe991c54403cdebdfa13
Gabriela <othergabs@gmail.com>
2021-11-16 21:52:12 -0300

    base do sistema de artigos

--- index.js
+++ index.js
@@ -2,43 +2,10 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-// GET - obter algo
-// POST - criar algo
-// PUT - atualizar algo
-// DELETE - deletar algo
-
-// npm install --save-dev nodemon
-
 app.use(express.json());
 
 app.get("/", function(req, res) {
-    res.send("Minha primeira requisição");
-});
-
-app.get("/segunda-req", function(req, res) {
-    res.send("Minha SEGUNDA requisição");
-});
-
-app.get("/com-parametros", function (req, res) {
-    if (req.query.nome === "Gabriela") {
-        res.send("Gabriela chamou requisição");
-    }
-    res.send("Com parâmetros funciona! Sabadou " + req.query.nome + req.query.sobrenome);
-});
-
-app.post("/meu-primeiro-post", function (req, res) {
-    console.log(req.body);
-    res.send("Meu post funciona");
-});
-
-app.put("/meu-primeiro-put/:id", function (req, res) {
-    console.log(req.body, req.params.id);
-    res.send("Meu put funciona");
-});
-
-app.delete("/meu-primeiro-delete/:id", function (req, res) {
-    console.log(req.params.id);
-    res.send("Meu delete funciona " + req.params.id);
+    res.send("Dasa Educa - Artigos");
 });
 
 app.listen(port, function() {
---
+++ routes/artigos.routes.js
@@ -0,0 +1,14 @@
+// rotas do sistema de artigos
+
+/*
+    GET
+        - Obter todos os artigos
+        - Obter um artigo especifico
+        - Obter todos os artigos publicados
+    POST
+        - Criar um novo artigo
+    PUT
+        - Publicar meu artigo
+    DELETE
+        - Deletar um artigo
+*/

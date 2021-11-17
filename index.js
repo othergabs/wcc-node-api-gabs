03c43b7c7a97dd2c6979a96724bcd47e44e35a87
Gabriela <othergabs@gmail.com>
2021-11-17 20:13:40 -0300

    recapitulacao de requisicoes

--- index.js
+++ index.js
@@ -4,8 +4,27 @@ const port = 8080;
 
 app.use(express.json());
 
-app.get("/", function(req, res) {
-    res.send("Dasa Educa - Artigos");
+app.get("/", function(request, response) {
+    response.send("Dasa Educa - Artigos");
+});
+
+app.post("/", function(request, response) {
+    console.log(request.body);
+    response.send("Dasa Educa - Post");
+});
+
+app.put("/:id", function(request, response) {
+    // mensagem e titulo
+    console.log(request.body, request.body, request.params.id);
+    response.send("Dasa Educa - Put");
+});
+
+app.delete("/", function(request, response) {
+    response.send("Dasa Educa - Delete geral");
+});
+
+app.delete("/:id", function (request, response) {
+  response.send("Dasa Educa - Delete específico");
 });
 
 app.listen(port, function() {

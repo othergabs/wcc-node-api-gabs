3180658f874d9b7598b6252a9436d312a709baf8
Gabriela <gabriela.fonseca@idwall.co>
2021-11-19 20:41:56 -0300

    implementado endpoint findAll artigos

--- controllers/artigos.controller.js
+++ controllers/artigos.controller.js
@@ -4,21 +4,31 @@ const tabelaArtigos = database.artigos;
 
 // Cria um novo artigo
 exports.create = (request, response) => {
-    const artigo = {
-        titulo: request.body.titulo,
-        descricao: request.body.descricao,
-        publicado: request.body.publicado
-    };
+  const artigo = {
+    titulo: request.body.titulo,
+    descricao: request.body.descricao,
+    publicado: request.body.publicado,
+  };
+  // a promise pode ser resolvida
+  // ou ela pode ser rejeitada (exemplo: ocorreu um erro ao tentar salvar)
 
-    // a promise pode ser resolvida
-    // ou ela pode ser rejeitada (exemplo: ocorreu um erro ao tentar salvar)
-
-    tabelaArtigos.create(artigo)
+  tabelaArtigos
+    .create(artigo)
     .then(function () {
-        response.send("Artigo criado com sucesso");
+      response.send("Artigo criado com sucesso");
     })
     .catch(function (error) {
-      console.log(error);
       response.status(500).send("Ocorreu um erro ao salvar o artigo");
+    });
+};
+
+exports.findAll = (request, response) => {
+  tabelaArtigos
+    .findAll()
+    .then(function (data) {
+      response.send(data);
     })
+    .catch(function () {
+      response.status(500).send("Ocorreu um erro aos buscar todos os artigos.");
+    });
 };
